10d4e6d609cf32594e0ac0fb1f82cbc5b8a80838
Marcio Lisboa <mjlbnu@gmail.com>
2021-03-25 22:11:05 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '424bf25f843e8bea3675d9429906f231'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd2c9cf16-e720-4274-b3c7-088409f64801'
+};
